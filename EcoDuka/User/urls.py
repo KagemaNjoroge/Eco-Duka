0e5c4f8df3d1188982a86db36773897cb9a36214
Kagema Njoroge <137799358+KagemaNjoroge@users.noreply.github.com>
2024-02-28 23:57:51 +0300

    Add user and notifications URLs

--- EcoDuka/User/urls.py
+++ EcoDuka/User/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
 
 
-from .views import LocationView
+from .views import LocationView, UserView, NotificationsView
 
 urlpatterns = [
     path("location/", LocationView.as_view(), name="location"),
     path("location/<int:id>/", LocationView.as_view(), name="location"),
+    path("user/", UserView.as_view(), name="user"),
+    path("user/<int:id>/", UserView.as_view(), name="user"),
+    path("notifications/", NotificationsView.as_view(), name="notifications"),
+    path("notifications/<int:id>/", NotificationsView.as_view(), name="notification"),
 ]
