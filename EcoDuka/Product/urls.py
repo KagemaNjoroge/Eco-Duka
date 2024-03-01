f750906c1fb26283e059726ded7c32b340ae8e7b
Kagema Njoroge <137799358+KagemaNjoroge@users.noreply.github.com>
2024-03-01 19:28:19 +0300

    Add search functionality to product URLs

--- EcoDuka/Product/urls.py
+++ EcoDuka/Product/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from .views import ProductView, CategoryView
+from .views import ProductView, CategoryView, search
 
 urlpatterns = [
     path("product/", ProductView.as_view(), name="product"),
     path("product/<int:id>/", ProductView.as_view(), name="product"),
     path("category/", CategoryView.as_view(), name="category"),
     path("category/<int:id>/", CategoryView.as_view(), name="category"),
+    path("search/", search, name="search"),
 ]
